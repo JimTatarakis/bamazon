5df87a3867cac49d6c40f83b116ce2f710a2e54b
Jim Tatarakis <jimtatarakis@gmail.com>
2019-06-03 19:48:39 -0700

    app working, just need to finish readme

--- node.js/bamazon_customer.js
+++ node.js/bamazon_customer.js
@@ -18,30 +18,67 @@ var connection = mysql.createConnection({
     password: keys.login.pass,
     database: "bamazon_db"
 });
+// runs mySQL connection
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
 
 // the prompt using inquirer
 inquirer.prompt([
     {
         // prompt: asks what item id you want
-      name: "product",
-      message: "what's the product id of the item you want to buy?"
+        name: "product",
+        message: "what's the product id of the item you want to buy?"
     }, {
         // prompt: asks how many you want
-      name: "quantity",
-      message: "How many would you like?"
+        name: "quantity",
+        message: "How many would you like?"
     }
-  ]).then(function(answers) {
+]).then(function (answers) {
     var order = {
         product: answers.product,
         quantity: answers.quantity
     }
-    console.log(order);
-  });
+    // log order
+    console.log('Your order has been placed!');
 
-// take in answers.
+    // make order for db
+    function productOrdered() {
+        connection.query("SELECT * FROM products WHERE item_id = ?", VALUES = answers.product, function (err, res) {
+            if (err) {
+                console.log('item may not be avaialable. please try another item..')
+                throw err;
+            }
+            // check if item has enough stock
+            var newStock = res[0].stock_quantity - order.quantity;
 
-// use answers to check db for quantities
+            // make a variable for the total amount of order
+            var orderTotal = order.quantity * res[0].price;
 
-// respond based on db check
+            // respond based on db check
+            if (newStock < 0) {
+                console.log('not enough inventory at the moment!');
+            }
 
-// log purchase and update db
+            else {
+                // update item inventory
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: newStock
+                        },
+                        {
+                            item_id: order.product
+                        }
+                    ]
+                );
+                // log purchase with cost to customer
+                console.log('Your total today is $ ' + orderTotal + '. \nThank you for your patronage!');
+            };
+
+            connection.end();
+        });
+    }
+    productOrdered();
+});
